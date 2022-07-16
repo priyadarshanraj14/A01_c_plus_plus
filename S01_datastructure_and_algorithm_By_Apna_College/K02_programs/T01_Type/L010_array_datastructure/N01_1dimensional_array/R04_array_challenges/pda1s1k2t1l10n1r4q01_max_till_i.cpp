4b37b113aa2d6549bd533e646e8242594c96c665
priyadarshanraj14 <priyadarshanraj14@gmail.com>
2022-07-16 16:25:52 +0530

    c

---
+++ S01_datastructure_and_algorithm_By_Apna_College/K02_programs/T01_Type/L010_array_datastructure/N01_1dimensional_array/R04_array_challenges/pda1s1k2t1l10n1r4q01_max_till_i.cpp
@@ -0,0 +1,25 @@
+//(L10)array data structure // (N1) 1_dimensional_array // (R4) array challenges
+//(Q1):- (max till i) given an array a[] of size n . for every i from 0 to n-1 output max 
+// ( a[0],a[1],a[2],.....a[i]).
+#include<iostream>
+#include<climits>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"how many number you will give=";
+    cin>>n;
+    int arr[n];
+    cout<<"enter your number\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int maximum=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        maximum=max(maximum,arr[i]);
+        cout<<maximum<<" ";
+    }
+    return 0;
+}
---
+++ S01_datastructure_and_algorithm_By_Apna_College/K02_programs/T01_Type/L010_array_datastructure/N01_1dimensional_array/R04_array_challenges/pda1s1k2t1l10n1r4q02_all_subarray_sum.cpp
@@ -0,0 +1,32 @@
+//(L10)array data structure // (N1) 1_dimensional_array // (R4) array challenges
+//(Q1):-(All subarray sum). Given an array a[] of size n, output sum of each 
+//subarray of the given array.
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"how many number you will give=";
+    cin>>n;
+    int arr[n];
+    cout<<"enter your number\n";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        int currentsum=0;
+        for(int j=i;j<n;j++)
+        {
+            currentsum+=arr[j];
+            cout<<"{";
+            for(int k=i;k<=j;k++)
+            {
+                cout<<arr[k]<<",";
+            }
+            cout<<"}="<<currentsum<<endl;
+        }
+    }
+    return 0;
+}
